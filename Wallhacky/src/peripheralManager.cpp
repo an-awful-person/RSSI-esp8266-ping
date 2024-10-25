98927d07b32682f1bdf9f490e07190a93994c226
Patrick Scheper <patrick.scheper@nlr.nl>
2024-10-25 13:56:30 +0200

    REMOVE upload code because it broke

--- Wallhacky/src/peripheralManager.cpp
+++ Wallhacky/src/peripheralManager.cpp
@@ -89,54 +89,47 @@ String PeripheralManager::GetPeripheralsInfo() {
      return result;
 }
 
-
-#include <HTTPClient.h>
-#include <WiFiClient.h>
-#include <WiFiType.h>
-
-#include <ArduinoJson.h>
-
 void PeripheralManager::PostToApi(WifiConfig wifiConfig, String url){
 
-    if(WiFi.status() == WL_CONNECTED){
-    WiFiClient client;
-    HTTPClient http;
+//     if(WiFi.status() == WL_CONNECTED){
+//     WiFiClient client;
+//     HTTPClient http;
 
-    http.begin(client, url);
-    http.addHeader("Content-Type", "application/json");
+//     http.begin(client, url);
+//     http.addHeader("Content-Type", "application/json");
 
-    StaticJsonDocument<256> jsonDoc;
-    jsonDoc["macAddress"] = WiFi.macAddress();
-    JsonArray pingArray = jsonDoc.createNestedArray("networkPings");
+//     StaticJsonDocument<256> jsonDoc;
+//     jsonDoc["macAddress"] = WiFi.macAddress();
+//     JsonArray pingArray = jsonDoc.createNestedArray("networkPings");
 
 
-    for(int i=0; i<peripheralInfos.size(); ++i)
-    {
-        PeripheralInfo peripheralDevice = peripheralInfos[i];
-        JsonObject networkPing = pingArray.createNestedObject();
-        networkPing["ssid"] = peripheralDevice.GetAddress();
-        networkPing["bssid"] = peripheralDevice.GetManufacturer();
-        networkPing["rssi"] = peripheralDevice.GetRSSI();
-    }
+//     for(int i=0; i<peripheralInfos.size(); ++i)
+//     {
+//         PeripheralInfo peripheralDevice = peripheralInfos[i];
+//         JsonObject networkPing = pingArray.createNestedObject();
+//         networkPing["ssid"] = peripheralDevice.GetAddress();
+//         networkPing["bssid"] = peripheralDevice.GetManufacturer();
+//         networkPing["rssi"] = peripheralDevice.GetRSSI();
+//     }
 
-    String jsonPayload;
-    serializeJson(jsonDoc, jsonPayload);
+//     String jsonPayload;
+//     serializeJson(jsonDoc, jsonPayload);
 
-    Serial.println("Sending networkscan...");
-    int httpResponseCode = http.POST(jsonPayload);
+//     Serial.println("Sending networkscan...");
+//     int httpResponseCode = http.POST(jsonPayload);
 
-        // Check the response
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("HTTP Response code: " + String(httpResponseCode));
-      Serial.println("Response: " + response);
-    } else {
-      Serial.println("Error on sending POST: " + String(httpResponseCode));
-      Serial.println("url: "+ url);
-      Serial.println(jsonPayload);
-    }
+//         // Check the response
+//     if (httpResponseCode > 0) {
+//       String response = http.getString();
+//       Serial.println("HTTP Response code: " + String(httpResponseCode));
+//       Serial.println("Response: " + response);
+//     } else {
+//       Serial.println("Error on sending POST: " + String(httpResponseCode));
+//       Serial.println("url: "+ url);
+//       Serial.println(jsonPayload);
+//     }
 
-    http.end();  // Free resources
-  }
+//     http.end();  // Free resources
+//   }
 }
 
